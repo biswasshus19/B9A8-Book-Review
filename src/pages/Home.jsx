7ac6e7ecafd47e5ab7c48a5c1e01759e58507f4c
biswasshus19 <shusmitatonu6@gmail.com>
2024-04-09 21:20:41 +0600

    alsot complete book review

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,1 @@
-import Banner from "../components/Banner";
-import Books from "../components/Books";
 
-const Home = () => {
-  return (
-    <div>
-      <Banner></Banner>
-      <Books></Books>
-    </div>
-  );
-};
-
-export default Home;
